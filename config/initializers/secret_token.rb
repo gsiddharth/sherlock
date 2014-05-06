1f6d4cfafc27d4923bb29fb06f21ebe2f8dee894
gsiddharth <siddharth.garg85@gmail.com>
2014-05-06 22:16:39 +0530

    first whole project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sherlock::Application.config.secret_key_base = 'f6040f4a83137c17d7c8d25d882bf636fde5dc59da51f889dc5683462a6e1e2a5ddd789152cd5ecb205855fac6e720f4d757a3f517d5f0f92a9ece81762c44d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sherlock::Application.config.session_store :cookie_store, key: '_sherlock_session'
